5ea3f281ac28a3fb48d7056e13537ab4399d5c7d
Sasindu Fernando <pujana.sasindu@gmail.com>
2023-11-04 16:12:06 +0530

    news feed bug fixed

--- lib/pages/news_feed.dart
+++ lib/pages/news_feed.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:helpmate/pages/listItems/list_items.dart';
 import 'package:toggle_switch/toggle_switch.dart';
 
 class NewsFeed extends StatefulWidget {
@@ -10,6 +11,45 @@ class NewsFeed extends StatefulWidget {
 
 class __NewsFeedState extends State<NewsFeed> {
   int _selectedIndex = 0;
+
+  List<NotificationItem> dummyData = [
+    NotificationItem(
+      title: 'Notification 1',
+      message: 'This is a sample notification message.',
+      time: '2 minutes ago',
+      imagePath: 'assets/image_1.png', // Path to the image for notification 1
+    ),
+    NotificationItem(
+      title: 'Notification 2',
+      message: 'Another notification message.',
+      time: '5 minutes ago',
+      imagePath: 'assets/image_2.png', // Path to the image for notification 2
+    ),
+    NotificationItem(
+      title: 'Notification 2',
+      message: 'Another notification message.',
+      time: '5 minutes ago',
+      imagePath: 'assets/image_3.png', // Path to the image for notification 2
+    ),
+    NotificationItem(
+      title: 'Notification 2',
+      message: 'Another notification message.',
+      time: '5 minutes ago',
+      imagePath: 'assets/image_4.png', // Path to the image for notification 2
+    ),
+    NotificationItem(
+      title: 'Notification 2',
+      message: 'Another notification message.',
+      time: '5 minutes ago',
+      imagePath: 'assets/image_5.png', // Path to the image for notification 2
+    ),
+    NotificationItem(
+      title: 'Notification 2',
+      message: 'Another notification message.',
+      time: '5 minutes ago',
+      imagePath: 'assets/image_6.png', // Path to the image for notification 2
+    ),
+  ];
 
   List<Color> _getActiveColor(int index) {
     return [
@@ -72,8 +112,28 @@ class __NewsFeedState extends State<NewsFeed> {
             bottom: 0,
             child: Offstage(
               offstage: _selectedIndex != 0,
-              child: const Center(
-                child: Text('News Feed'), // Your "News Feed" content
+              child: ListView.builder(
+                itemExtent: 100,
+                itemCount: dummyData.length,
+                itemBuilder: (context, index) {
+                  final notification = dummyData[index];
+                  return ListTile(
+                    leading: Image.asset(
+                      notification
+                          .imagePath, // Use the image path from the data
+                      width: 100, // Adjust the width as needed
+                      height: 130, // Adjust the height as needed
+                    ),
+                    title: Text(notification.title),
+                    subtitle: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(notification.message),
+                        Text(notification.time),
+                      ],
+                    ),
+                  );
+                },
               ),
             ),
           ),
